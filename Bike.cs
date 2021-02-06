451b38b08ba24ca2715f5521f8ba9dc5371edd41
Seb1903 <sebastien.martinez99@gmail.com>
2021-02-06 22:38:25 +0100

    Create basic bike classes AB#49

---
+++ Bike.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bovelo
+{
+    public abstract class Bike
+    {
+        public int serial_number; 
+        public List<Part>;  //create Part 
+        public int price; 
+    }
+    public class Roadbike : Bike
+    {
+
+    }
+    public class Explorer : Roadbike
+    {
+
+    }
+
+    public class City : Roadbike
+    {
+
+    }
+
+    public class Adventure : Bike
+    {
+
+    }
+}
+
