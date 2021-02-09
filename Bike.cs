2fc2bb02101685178198b65d7a37447e1e0b8935
Seb1903 <sebastien.martinez99@gmail.com>
2021-02-09 09:01:42 +0100

    Add constructors

--- Bike.cs
+++ Bike.cs
@@ -7,22 +7,29 @@ using System.Drawing;
 
 namespace Bovelo
 {
-    public abstract class Bike    
+    public abstract class Bike
     {
-        public int serial_number; 
-        public List<Part> PartList = new List<Part>() ;  //create Part 
+        public int serial_number;
+        public List<Part> PartList = new List<Part>();  //create Part 
         public int price;
 
-        public Frame frame ;    
+        public Frame frame;
         public Tire tire;
         public Basic_Kit Basic_Kit;
+        
+        public Bike(Frame frame, Tire tire, Basic_Kit basic_Kit)
+        {
+            this.frame = frame;
+            this.tire = tire;
+            this.Basic_Kit = basic_Kit; 
+        }
     }
     public class Roadbike : Bike
     {
         public Luggage_rack luggage_rack ;
         public Lighting lighting ;
         public Mudguard mudguard ;
-        public Roadbike(int size, Color color) 
+        public Roadbike(int size, Color color, Frame frame, Tire tire, Basic_Kit basic_Kit, Luggage_rack luggage_Rack, Lighting lighting, Mudguard mudguard) : base(frame,tire,basic_Kit)
         {
             this.frame.color = color;
             this.frame.size = size;
@@ -30,7 +37,7 @@ namespace Bovelo
     }
     public class Explorer : Roadbike
     {
-        public Explorer(int size, Color color) : base(size,color)
+        public Explorer(int size, Color color, Frame frame, Tire tire, Basic_Kit basic_Kit, Luggage_rack luggage_Rack, Lighting lighting, Mudguard mudguard) : base(size, color, frame, tire, basic_Kit,luggage_Rack,lighting,mudguard)
         {
             this.tire.size = 2;     // 1 = normal and 2 = large 
             this.tire.grooves = "very groovy"; // to be changed 
@@ -40,7 +47,7 @@ namespace Bovelo
 
     public class City : Roadbike
     {
-        public City(int size, Color color) : base(size, color)
+        public City(int size, Color color, Frame frame, Tire tire, Basic_Kit basic_Kit, Luggage_rack luggage_Rack, Lighting lighting, Mudguard mudguard) : base(size, color, frame, tire, basic_Kit, luggage_Rack, lighting, mudguard)
         {
             this.tire.size = 1; 
             //necessary to define specific values for mudguard type and tire grooves ?
@@ -50,7 +57,7 @@ namespace Bovelo
 
     public class Adventure : Bike
     {
-        public Adventure(int size, Color color)
+        public Adventure(int size, Color color,Frame frame, Tire tire, Basic_Kit basic_Kit) : base(frame, tire, basic_Kit)
         {
             this.frame.color = color;
             this.frame.size = size;
