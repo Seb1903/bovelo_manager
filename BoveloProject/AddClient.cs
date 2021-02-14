334681119b67d166194e1f3ff61e8edfb99466bf
Seb1903 <sebastien.martinez99@gmail.com>
2021-02-14 18:22:26 +0100

    Complet client adding form, fully functionnal

--- BoveloProject/AddClient.cs
+++ BoveloProject/AddClient.cs
@@ -16,6 +16,41 @@ namespace Bovelo
         public AddClient()
         {
             InitializeComponent();
+            show_table();
+        }
+
+        private void show_table()
+        {
+            Database db1 = new Database();
+            MySqlConnection conn = new MySqlConnection(db1.MyConnection);
+       
+            try
+            {
+                Console.WriteLine("Connecting to MySQL...");
+                conn.Open();
+                Console.WriteLine("Connected");
+
+                string sql = "SELECT * FROM table_client;";
+
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                MySqlDataReader rdr = cmd.ExecuteReader();
+
+                label_db_clients.Items.Clear();
+
+                while (rdr.Read())
+                {
+                    
+                    label_db_clients.Items.Add(rdr[0] + " -- " + rdr[1]);
+                }
+                rdr.Close();
+            }
+
+            catch (Exception ex)
+
+            {
+                label_first_name.Text = ex.ToString();
+            }
+            conn.Close();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -26,7 +61,7 @@ namespace Bovelo
 
                 //This is my connection string i have assigned the database file address path  
                 //This is my insert query in which i am taking input from the user through windows forms  
-                string Query = "insert into table_client(id_client,client_lastname,client_firstname,client_country,client_city,client_street,client_number,client_zipcode,client_phoneNumber,client_emailAddress) values('" + this.textBox1.Text + "','" + this.textBox2.Text + "','" + this.textBox3.Text + "','" + this.textBox4.Text + "','" + this.textBox5.Text + "','" + this.textBox6.Text + "','" + this.textBox7.Text + "','" + this.textBox8.Text + "','" + this.textBox9.Text + "','" + this.textBox10.Text + "');";
+                string Query = "insert into table_client(client_lastname,client_firstname,client_country,client_city,client_street,client_number,client_zipcode,client_phoneNumber,client_emailAddress) values('" + this.textBox_lastname.Text + "','" + this.textBox_firstname.Text + "','" + this.textBox_country.Text + "','" + this.textBox_city.Text + "','" + this.textBox_street.Text + "','" + this.numericUpDown1.Value + "','" + this.numericUpDown_zipcode.Value + "','" + this.label_phonenumber.Text + "','" + this.label_email.Text + "');";
                 //This is  MySqlConnection here i have created the object and pass my connection string.  
                 MySqlConnection MyConn2 = new MySqlConnection(db.MyConnection);
                 //This is command class which will handle the query and connection object.  
@@ -45,52 +80,12 @@ namespace Bovelo
                 MessageBox.Show(ex.Message);
             }
 
+            show_table();
+
             
 
-            Database db1 = new Database();
-            MySqlConnection conn = new MySqlConnection(db1.MyConnection);
-
-            try
-
-            {
-
-                Console.WriteLine("Connecting to MySQL...");
-
-                conn.Open();
-
-                Console.WriteLine("Connected");
-
-                string sql = "SELECT * FROM table_client;";
-
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-
-                MySqlDataReader rdr = cmd.ExecuteReader();
-
-
-
-                while (rdr.Read())
-
-                {
-                    listBox1.Items.Add(rdr[0] + " -- " + rdr[1]);
-                }
-
-                rdr.Close();
-
-               
-
             }
 
-            catch (Exception ex)
-
-            {
-                label1.Text = ex.ToString();
-            }
-
-            conn.Close();
-            label1.Text = "Done";
-
-            }
-
-    
+        
     }
 }
