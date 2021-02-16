ecab1638a60bc21a0fe525f32f7bc3a839e9b1b3
MNiko97 <nikola.mitrovic@student.ecam.be>
2021-02-16 22:04:41 +0100

    Show all bikes category in BasketForm.cs Add remove bike option cart Add change bike quantity option from cart Add a check empty cart function to alert the user Add a refresh form function to update the content in real time AB#208

--- BoveloProject/BasketForm.cs
+++ BoveloProject/BasketForm.cs
@@ -16,28 +16,37 @@ namespace Bovelo
         {
             InitializeComponent();
         }
-
         private void BasketForm_Load(object sender, EventArgs e)
         {
+            showBasket();      
+        }
+        private void showBasket()
+        {
             int position = 1;
-            foreach(BasketItem item in CatalogForm.basket)
+            foreach (BasketItem item in CatalogForm.basket)
             {
                 Label bikeCategoryLbl = new Label();
                 NumericUpDown quantityBtn = new NumericUpDown();
                 Button removeBtn = new Button();
 
                 bikeCategoryLbl.Text = item.category;
-                bikeCategoryLbl.Top = position * 25;
+                bikeCategoryLbl.Top = position * 20 + 10;
                 bikeCategoryLbl.Left = 10;
+                bikeCategoryLbl.Size = new Size(80, 20);
 
                 quantityBtn.Value = item.quantity;
-                quantityBtn.Top = position * 25;
-                quantityBtn.Left = 200;
+                quantityBtn.Top = position * 20 + 10;
+                quantityBtn.Left = 100;
+                quantityBtn.Minimum = 0;
+                quantityBtn.Maximum = 100;
+                quantityBtn.Size = new Size (50, 20);
+                quantityBtn.ValueChanged += (sender, EventArgs) => { quantity_scroll(sender, EventArgs, item); };
 
                 removeBtn.Text = "remove";
-                removeBtn.Top = position * 25;
-                removeBtn.Left = 400;
-                removeBtn.Click += new EventHandler(this.removeBtn_Click, item);
+                removeBtn.Top = position * 20 + 10;
+                removeBtn.Left = 170;
+                removeBtn.Size = new Size(50, 20);
+                removeBtn.Click += (sender, EventArgs) => { removeBtn_Click(sender, EventArgs, item); };
 
                 position++;
 
@@ -45,25 +54,61 @@ namespace Bovelo
                 this.Controls.Add(quantityBtn);
                 this.Controls.Add(removeBtn);
             }
-            
+        }
+        private void UpdateForm()
+        {
+            this.Controls.Clear();
+            this.InitializeComponent();
+            showBasket();
+        }
+        private void CheckEmptyCart()
+        {
+            if (CatalogForm.basket.Count == 0)
+            {
+                DialogResult result = MessageBox.Show("Your cart is empty! Would you like to go back to catalog?", "Cart Info", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    this.Close();
+                    this.DialogResult = DialogResult.OK;
+                }
+            }
         }
         private void removeBtn_Click(object sender, EventArgs e, BasketItem item)
         {
-            Button button = sender as Button;
-            BasketItem itemr = item as BasketItem;
-            
-            CatalogForm.basket.Remove(itemr); Console.WriteLine("removed item");
-                    
+            CatalogForm.basket.Remove(item); 
+            Console.WriteLine("Item Removed");
+            UpdateForm();
+            CheckEmptyCart();
+        }
+        private void quantity_scroll(object sender, EventArgs e, BasketItem item)
+        {
+            NumericUpDown quantityBtn = sender as NumericUpDown;
+            int newQuantity = Decimal.ToInt32(quantityBtn.Value);
+            if (newQuantity == 0)
+            {
+                CatalogForm.basket.Remove(item);
+                Console.WriteLine("Item Removed");
+                UpdateForm();
+            }
+            else
+            {
+                item.quantity = newQuantity;
+                Console.WriteLine("New item quantity : " + item.quantity.ToString());
+            }
+            CheckEmptyCart();
         }
         private void back_Click(object sender, EventArgs e)
         {
-            this.Hide();
+            this.Close();
             this.DialogResult = DialogResult.OK;
         }
-
         private void empty_cart_btn_Click(object sender, EventArgs e)
         {
             CatalogForm.basket.Clear();
+        }
+        private void validate_Click(object sender, EventArgs e)
+        {
+            // Confirm order
         }
     }
 }
