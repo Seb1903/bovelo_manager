c3b48fc66a1853ed32135061ecb047b06851cd23
MNiko97 <nikola.mitrovic@student.ecam.be>
2021-02-18 10:26:03 +0100

    Add BuyableItem class AB#262

---
+++ BoveloProject/BuyableItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bovelo
+{
+    public class BuyableItem
+    {
+        public string category { get; set; }
+        public string color { get; set; }
+        public string size { get; set; }
+        public int quantity { get; set; }
+
+        public BuyableItem(string category, string color, string size, int quantity)
+        {
+            this.category = category;
+            this.color = color;
+            this.size = size;
+            this.quantity = quantity;
+        }
+        public void AddToDb()
+        {
+            // db request
+        }
+
+    }
+}
