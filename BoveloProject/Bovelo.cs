5c82bd38ca66cdba279ac21363f57ba11a56f751
Seb1903 <sebastien.martinez99@gmail.com>
2021-02-18 11:36:14 +0100

    Add Bovelo class, acts as a controller AB#254

---
+++ BoveloProject/Bovelo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Bovelo
+{
+    public class Bovelo
+    {
+        Order order = new Order();
+        public Bovelo()
+        {
+        }
+        public void Run()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new CatalogForm());
+        }
+    }
+}
