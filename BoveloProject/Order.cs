9d27e7e8fc79bb4109b3dbb398789e204631295a
Tateuf <logan.noel.h@gmail.com>
2021-02-18 11:48:20 +0100

    Update Order.cs database

--- BoveloProject/Order.cs
+++ BoveloProject/Order.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using MySql.Data.MySqlClient;
 
 namespace Bovelo
 {
@@ -52,6 +53,36 @@ namespace Bovelo
         public void AddClient(Client client)
         {
             this.client = client;
+        }
+        public void Save()
+        {
+            try
+            {
+                Database db = new Database();   //Let's us protect the password by having a class database with a public string containing the credentials
+                foreach (BuyableItem item in content)
+                {
+                    //This is my connection string i have assigned the database file address path  
+                    //This is my insert query in which i am taking input from the user through windows forms  
+                    string Query = "insert into table_bike(bike_type,bike_color,bike_size) values('" + item.category + "','" + item.color + "','" + item.size + "');";
+                    //This is  MySqlConnection here i have created the object and pass my connection string.  
+                    MySqlConnection MyConn2 = new MySqlConnection(db.MyConnection);
+                    //This is command class which will handle the query and connection object.  
+                    MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                    MySqlDataReader MyReader2;
+                    MyConn2.Open();
+                    MyReader2 = MyCommand2.ExecuteReader();     // Here our query will be executed and data saved into the database.  
+
+                    Console.WriteLine("Save Data");
+                    while (MyReader2.Read())
+                    {
+                    }
+                    MyConn2.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
