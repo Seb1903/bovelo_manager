3e37d72ec6b49ccf996dcead54693934cdecc0ef
Seb1903 <sebastien.martinez99@gmail.com>
2021-02-23 23:19:34 +0100

    Improve first catalog appearance

--- BoveloProject/NewCatalogForm.Designer.cs
+++ BoveloProject/NewCatalogForm.Designer.cs
@@ -33,9 +33,9 @@ namespace Bovelo
             this.explorerBox = new System.Windows.Forms.PictureBox();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.adventureBox = new System.Windows.Forms.PictureBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.cityBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.explorerBox)).BeginInit();
             this.tableLayoutPanel1.SuspendLayout();
@@ -49,7 +49,7 @@ namespace Bovelo
             this.cityBox.Image = global::Bovelo.Properties.Resources.city_no_background;
             this.cityBox.Location = new System.Drawing.Point(4, 4);
             this.cityBox.Name = "cityBox";
-            this.cityBox.Size = new System.Drawing.Size(558, 648);
+            this.cityBox.Size = new System.Drawing.Size(558, 638);
             this.cityBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.cityBox.TabIndex = 0;
             this.cityBox.TabStop = false;
@@ -62,7 +62,7 @@ namespace Bovelo
             this.explorerBox.Image = global::Bovelo.Properties.Resources.explorer_no_background;
             this.explorerBox.Location = new System.Drawing.Point(569, 4);
             this.explorerBox.Name = "explorerBox";
-            this.explorerBox.Size = new System.Drawing.Size(558, 648);
+            this.explorerBox.Size = new System.Drawing.Size(558, 638);
             this.explorerBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.explorerBox.TabIndex = 1;
             this.explorerBox.TabStop = false;
@@ -77,19 +77,16 @@ namespace Bovelo
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
             this.tableLayoutPanel1.Controls.Add(this.adventureBox, 2, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 1);
-            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.cityBox, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.explorerBox, 1, 0);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(18, 18);
             this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1698, 972);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1698, 646);
             this.tableLayoutPanel1.TabIndex = 3;
             // 
             // adventureBox
@@ -99,56 +96,50 @@ namespace Bovelo
             this.adventureBox.Image = global::Bovelo.Properties.Resources.adventure_no_background;
             this.adventureBox.Location = new System.Drawing.Point(1134, 4);
             this.adventureBox.Name = "adventureBox";
-            this.adventureBox.Size = new System.Drawing.Size(560, 648);
+            this.adventureBox.Size = new System.Drawing.Size(560, 638);
             this.adventureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.adventureBox.TabIndex = 6;
             this.adventureBox.TabStop = false;
             this.adventureBox.Click += new System.EventHandler(this.adventureBox_Click);
             // 
-            // label3
+            // label1
             // 
-            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label3.Location = new System.Drawing.Point(1135, 680);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(558, 291);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Adventure";
-            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 709);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(566, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "City";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label2.Location = new System.Drawing.Point(570, 680);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(583, 709);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(556, 291);
-            this.label2.TabIndex = 4;
+            this.label2.Size = new System.Drawing.Size(562, 23);
+            this.label2.TabIndex = 5;
             this.label2.Text = "Explorer";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label1
+            // label3
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.Location = new System.Drawing.Point(5, 680);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(556, 291);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "City";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1148, 709);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(564, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Adventure";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // NewCatalogForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1734, 1083);
+            this.ClientSize = new System.Drawing.Size(1734, 771);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "NewCatalogForm";
             this.Text = "NewCatalogForm";
@@ -166,9 +157,9 @@ namespace Bovelo
         private System.Windows.Forms.PictureBox cityBox;
         private System.Windows.Forms.PictureBox explorerBox;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox adventureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
