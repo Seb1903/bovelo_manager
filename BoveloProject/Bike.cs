227511184e4ef18db358a0acf6a81cfb3ce5db73
MNiko97 <nikola.mitrovic@student.ecam.be>
2021-03-05 10:29:16 +0100

    Modify Bike class constructor with database Modify Part class constuctor with database Create dictionnary for parts inside Bike class AB#412

--- BoveloProject/Bike.cs
+++ BoveloProject/Bike.cs
@@ -16,57 +16,47 @@ namespace Bovelo
         public string size;
         public int price;
         public Dictionary<string, Part> partList = new Dictionary<string, Part>();
-
         public Bike(int id)
         {
             this.serial_number = id;
             string query1 = $"SELECT * FROM bike WHERE id={serial_number}";
             MySqlDataReader reader1 = GetData(query1);
-            this.type = reader1.GetString(1);
-            this.size = reader1.GetString(3);
-            this.color = reader1.GetString(2);
 
-            Console.WriteLine(type + " " + size + " " + color);
+            this.type = reader1.GetString(1);
+            this.color = reader1.GetString(2);
+            this.size = reader1.GetString(3);
 
             string query2 = $"SELECT * FROM model_parts WHERE model='{type}'";
             MySqlDataReader reader2 = GetData(query2);
-
-            
             for (int i = 1; i < reader2.FieldCount; i++)
             {
-                //Console.WriteLine("column name : " + reader2.GetName(i));
-                string query3 = $"SELECT * FROM part_stock WHERE name='{reader2.GetName(i)}'";
-                MySqlDataReader reader3 = GetData(query3);
-                Console.WriteLine("Result : " + reader3.GetValue(i).ToString());
-                //Console.WriteLine("Color : " + reader3.GetString(1));
-                /*if (reader3.GetString(1) != "Default") 
+                if(reader2[reader2.GetName(i)] != DBNull.Value)
                 {
                     Part part = new Part(reader2.GetName(i), color, reader2.GetString(i));
-                    partList.Add(reader2.GetName(i), part);
-                }
-                else
-                {
-                    Part part = new Part(reader2.GetName(i), "Default", reader2.GetString(i));
-                    partList.Add(reader2.GetName(i), part);
-                }*/   
-            }
-            // Print partList
-            Console.WriteLine("PARTLIST : ");
-            foreach(KeyValuePair<string, Part> part in partList)
-            {
-                Console.WriteLine("Key = {0}, Value = {1}", part.Key, part.Value);
+                    partList.Add(reader2.GetName(i), part);  
+                }             
             }
         }
         private static MySqlDataReader GetData(string query)
         {
-            Database db = new Database(); 
-            MySqlConnection MyConn = new MySqlConnection(db.MyConnection); 
-            MySqlCommand MyCommand = new MySqlCommand(query, MyConn);
-            MySqlDataReader MyReader;
-            MyConn.Open();
-            MyReader = MyCommand.ExecuteReader();
-            MyReader.Read();
-            return MyReader;
+            Database db = new Database();
+            MySqlConnection connection = new MySqlConnection(db.MyConnection);
+            MySqlCommand command = new MySqlCommand(query, connection);
+            MySqlDataReader reader;
+            connection.Open();
+            reader = command.ExecuteReader();
+            reader.Read();
+            return reader;
+        }
+        public override string ToString()
+        {
+            Console.WriteLine("\n-------------\nParts List:\n-------------");
+            foreach (KeyValuePair<string, Part> part in partList)
+            {
+                Console.WriteLine("\nType: {0} \n----Color = {1} \n----Characteristic = {2} \n----Quantity = {3}",
+                    part.Key, part.Value.color, part.Value.characteristic, part.Value.quantity);
+            }
+            return ("\n" + type + " " + size + " " + color);
         }
     }
 }
--- BoveloProject/Part.cs
+++ BoveloProject/Part.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
 using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace Bovelo
 {
@@ -24,37 +25,59 @@ namespace Bovelo
         public string color;
         public int quantity;
         public string characteristic;
-    
         public Part(string name, string color, string characteristic) 
         {
             this.name = name;
-            this.color = color;
             this.characteristic = characteristic;
-            string query = $"SELECT * FROM part_stock WHERE name={name} AND color={color} AND characteristic={characteristic}";
-            MySqlDataReader reader = GetData(query);
-            this.quantity = reader.GetInt32(2);
+            string colorQuery = $"SELECT * FROM part_stock WHERE name='{name}' AND characteristic='{characteristic}'";
+            DataTable colorTable = GetDataTable(colorQuery);
+            DataRow[] availableColor = colorTable.Select("color='" + color + "'");
+            if (availableColor.Length != 0)
+            {
+                this.color = color;
+            }
+            else
+            {
+                this.color = "Default";
+            }
+            string quantityQuery = $"SELECT * FROM part_stock WHERE name='{this.name}' AND color='{this.color}' AND characteristic='{this.characteristic}'";
+            DataTable quantityReader = GetDataTable(quantityQuery);
+            this.quantity = Convert.ToInt32(quantityReader.Rows[0]["quantity"].ToString());
         }
-
         public void Use()
         {
             quantity--;
-            string query = $"UPDATE part_stock SET quantity={quantity} WHERE name={name} AND color={color} AND characteristic={characteristic}";
+            string query = $"UPDATE part_stock SET quantity={quantity} WHERE name='{name}' AND color='{color}' AND characteristic='{characteristic}'";
+            ExecuteQuery(query);
         }
         public void Order(int quantity)
         {
             this.quantity += quantity;
-            string query = $"UPDATE part_stock SET quantity={quantity} WHERE name={name} AND color={color} AND characteristic={characteristic}";
+            string query = $"UPDATE part_stock SET quantity={this.quantity} WHERE name='{name}' AND color='{color}' AND characteristic='{characteristic}'";
+            ExecuteQuery(query);
         }
-        private static MySqlDataReader GetData(string query)
+        private static DataTable GetDataTable(string sqlCommand)
+        {
+            Database db1 = new Database();
+            MySqlConnection conn = new MySqlConnection(db1.MyConnection);
+            MySqlCommand command = new MySqlCommand(sqlCommand, conn);
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            adapter.SelectCommand = command;
+            DataTable table = new DataTable();
+            table.Locale = System.Globalization.CultureInfo.InvariantCulture;
+            adapter.Fill(table);
+            return table;
+        }
+        private static void ExecuteQuery(string query)
         {
             Database db = new Database();
-            MySqlConnection MyConn = new MySqlConnection(db.MyConnection);
-            MySqlCommand MyCommand = new MySqlCommand(query, MyConn);
-            MySqlDataReader MyReader;
-            MyConn.Open();
-            MyReader = MyCommand.ExecuteReader();
-            MyReader.Read();
-            return MyReader;
+            MySqlConnection connection = new MySqlConnection(db.MyConnection);
+            MySqlCommand command = new MySqlCommand(query, connection);
+            MySqlDataReader reader;
+            connection.Open();
+            reader = command.ExecuteReader();
+            while (reader.Read()) { }
+            connection.Close();
         }
     }
 }
